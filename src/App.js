0f4b9ca01b51146494eec0b5174c08cf13a42f86
Joe Sak <joe@joesak.com>
2017-01-23 20:16:48 -0600

    Add fahrenheit controlled input with some improved style

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ class App extends Component {
 
     this.state = {
       celsius: "",
+      fahrenheit: "",
     };
 
     this.onCelsiusChange = this.onCelsiusChange.bind(this);
+    this.onFahrenheitChange = this.onFahrenheitChange.bind(this);
   }
 
   onCelsiusChange(evt) {
@@ -20,13 +22,33 @@ class App extends Component {
     });
   }
 
+  onFahrenheitChange(evt) {
+    evt.preventDefault();
+
+    this.setState({
+      fahrenheit: evt.target.value,
+    });
+  }
+
   render() {
     return (
       <div className="App">
+        <h1>Convert between Celsius and Fahrenheit</h1>
+
         <input
           type="number"
           onChange={this.onCelsiusChange}
           value={this.state.celsius}
+          placeholder="°C"
+        />
+        <span className="fa fa-arrow-left"></span>
+        to
+        <span className="fa fa-arrow-right"></span>
+        <input
+          type="number"
+          onChange={this.onFahrenheitChange}
+          value={this.state.fahrenheit}
+          placeholder="°F"
         />
       </div>
     );

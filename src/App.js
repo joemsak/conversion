85d1d7d8e88f285fbc751c9ac7adf5a224199507
Joe Sak <joe@joesak.com>
2017-01-23 20:03:06 -0600

    Control the celsius input

--- src/App.js
+++ src/App.js
@@ -2,9 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      celsius: "",
+    };
+
+    this.onCelsiusChange = this.onCelsiusChange.bind(this);
+  }
+
+  onCelsiusChange(evt) {
+    evt.preventDefault();
+
+    this.setState({
+      celsius: evt.target.value,
+    });
+  }
+
   render() {
     return (
       <div className="App">
+        <input
+          type="number"
+          onChange={this.onCelsiusChange}
+          value={this.state.celsius}
+        />
       </div>
     );
   }
